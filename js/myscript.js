6f580191fb3241b792e1a6eb041eef84bd59be8f
akkagi0416 <akkagi0416@gmail.com>
2015-05-29 18:30:58 +0900

    mobile design fix

--- js/myscript.js
+++ js/myscript.js
@@ -1,13 +1,20 @@
 jQuery(function( $ ){
-    /*
-    $('.search').toggle(function(){
-        $('.searchform').css('display', 'block');
-    },
-    function(){
-        $('.searchform').css('display', 'none');
-    });
-    */
+    /* search form display on/off */
     $('.search').click(function(){
         $('.searchform').toggle(200);
+    });
+
+    /* goto top */
+    $('.pagetop').hide();
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 100){
+            $('.pagetop').fadeIn();
+        }else{
+            $('.pagetop').fadeOut();
+        }
+    });
+    $('.pagetop').click(function(){
+        $('html,body').animate({scrollTop:0}, '500');
+        return false;
     });
 });
